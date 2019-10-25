5e0553fbd70b657931ed9ecde4327206e3090c30
Daniel <43968979+DANSKI423319@users.noreply.github.com>
2019-10-25 17:21:17 +0100

    Commit 3: Home page and style sheet.
    Home page and style sheet made for The Cozy Tea Room enterprise.

---
+++ CSS_StyleSheet.php
@@ -0,0 +1,58 @@
+<?php
+?>
+
+<html>
+    <body>
+        <style>
+            .body{
+                font-family: arial;
+                margin: 20px;
+            }
+            
+            .h2{
+                color: darkcyan;
+            }
+                 
+            .formLayout{
+                border-radius: 5px;
+                background-color: #f2f2f2;
+                padding: 20px;
+                width:428px;
+            }
+            
+            .input[type=text]{
+                width: 15%;
+                padding: 14px 20px;
+                margin: 8px 0;
+                display: inline-block;
+                border: 1px solid #ccc;
+                border-radius: 4px;
+                text-align: center;
+            }
+            
+            .buttonEnterNumber{
+                background-color: darkcyan;
+                font-size: 16px;
+                transition: 0.3s;
+                opacity: 0.8;
+                padding: 12px 50px;
+                border: none;
+                color: white;
+            }
+            
+            .buttonLogin{
+                background-color: #ee3e80;
+                font-size: 16px;
+                transition: 0.3s; 
+                opacity: 0.8;
+                padding: 12px 50px;
+                border: none;
+                color: white;
+            }
+            
+            .buttonLogin:hover {opacity: 1};
+            .buttonEnterNumber:hover {opacity: 1};
+
+        </style>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -1,9 +1,23 @@
-<html>
-<body>
-
-<?php
-echo "This is for my ISAD251 Coursework!";
+<?php 
+require_once 'CSS_StyleSheet.php'; 
 ?>
 
-</body>
+<html>
+    
+    <body class="body">
+
+        <h2 class="h2">
+            The Cozy Tea Room
+        </h2> 
+
+        <form class="formLayout" name="TableInput">
+            <b>Table Number:</b><br/>
+            <input type="text" name="tableNumber" placeholder="###" class="input" maxlength="3">
+            <input type="button" value="Enter new order" class="buttonEnterNumber">
+            <input type="button" value="Login" class='buttonLogin'>
+        </form>
+    
+    </body>
+
 </html>
+
