f20b323dd576727715777fe8789aff6d33b06efd
Daniel <43968979+DANSKI423319@users.noreply.github.com>
2019-11-06 17:45:21 +0000

    Commit 12
    Commented out main connection method in dbConnection.php. Replaced with experimental value and will have to confirm with peers whether it would be acceptable.

--- dbConnection.php
+++ dbConnection.php
@@ -6,31 +6,47 @@ const DB_PASSWORD = 'ISAD251_22205389';
 const DB_DATABASE = 'ISAD251_DSkillman';
 
 
-function getConnection() {
+//function getConnection() {
+//
+//    $dataSrcName = 'mysql:dbname='.DB_DATABASE.';host='.DB_SERVER;
+//    $dbConnection = null;
+//    try
+//    {
+//        $dbConnection = new PDO($dataSrcName, DB_USER, DB_PASSWORD);
+//    } catch (PDOException $err) {
+//        echo 'Connection failed: ', $err->getMessage();
+//    }
+//    return $dbConnection;
+//}
 
-    $dataSrcName = 'mysql:dbname='.DB_DATABASE.';host='.DB_SERVER;
-    $dbConnection = null;
-    try
-    {
-        $dbConnection = new PDO($dataSrcName, DB_USER, DB_PASSWORD);
-    } catch (PDOException $err) {
-        echo 'Connection failed: ', $err->getMessage();
+//function getTable($tablename) {
+//    $statement = getConnection()->prepare("SELECT * FROM".$tablename);
+//    $statement->execute();
+//    $resultSet = $statement ->fetchTable(PDO::FETCH_ASSOC);
+//    
+//    return $resultSet;
+//}
+
+$dbConn = new mysqli(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE);
+if($dbConn->connect_error) {
+    die("Connection failed:". $dbConn->connect_error);
+}
+
+$sql = "SELECT userID, username, userPassword, userType FROM trusers";
+$output = $dbConn->query($sql);
+
+if ($output->num_rows > 0) {
+    while($row = $output->FETCH_ASSOC()) {
+        echo "UserID: " . $row["userID"] . " / Username: " . $row["username"] . 
+                " / Password: " . $row["userPassword"] . " / User Type: " . 
+                $row["userType"] . ".<br>";
     }
-    return $dbConnection;
-}
+    }else {
+        echo "0 reults";
+    }
 
-//$sql = "SELECT userID, username, userPassword, userType FROM trusers";
-//$tableOut = getConnection()->query($sql);
-
-function getTable($tablename) {
-    $statement = getConnection()->prepare("SELECT * FROM".$tablename);
-    $statement->execute();
-    $resultSet = $statement ->fetchTable(PDO::FETCH_ASSOC);
     
-    return $resultSet;
-}
 
-echo 'all excecuted';
 //
 //if ($tableOut->num_rows > 0) {
 //    // output data of each row
