ddaf1ec52f13b3d2fd07ba3962764d4731ce3228
Daniel <43968979+DANSKI423319@users.noreply.github.com>
2019-11-26 15:00:05 +0000

    Commit 22.1
    Added reminder comment.

--- dbMenu.php
+++ dbMenu.php
@@ -1,10 +1,10 @@
+<script type="text/javascript" src="menuFunctions.js"></script>
+
 <?php
 include_once 'assets/header.php';
 include_once 'dbConnection.php';
-include_once 'menuFunctions.php';
 
 $tableName = 'trMenu';
-$TEST = 'Tea';
 
 $tblOutput = getAll($tableName);
 
@@ -31,7 +31,7 @@ if ($tblOutput) {
                 <td>' . $tblRow['itemName'] . '</td>
                 <td>' . $tblRow['itemDesc'] . '</td> .
                 <td>£' . $tblRow['itemPrice'] . '</td>
-                <td>'
+                <td>' /* ADD VALUE OR ID TO EACH ROW, TO EXTRACT VALUES */
         ?>
         <input class="btn-success" id="btnAdd" type="button" value="+" onclick="onClickAdd()">
         <input class="btn-danger" id="btnSub" type="button" value="-" onclick="onClickSub()">
@@ -40,11 +40,7 @@ if ($tblOutput) {
     }
 }
 
-echo '</tr></tbody></table>            
-<div class="panel panel-default">
-    <div class="panel-heading"><b>Basket</b></div>
-    <div class="panel=body">Basket Content</div>
-</div>';
+echo '</tr></tbody></table>';
 
 echo '<pre>';
 print_r($tblOutput);
